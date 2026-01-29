574307373145885898c99d26a2580ddb146100f6
MurugeshAravind <arvindh.balasubramaniam@gmail.com>
2026-01-29 23:47:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import TicTacToe from './components/TicTacToe';
+import './App.css';
+
+function App() {
+  return (
+    <TicTacToe boardSize={4} />
+  )
+}
+
+export default App
---
+++ src/components/TicTacToe.tsx
@@ -0,0 +1,21 @@
+import useTicTacToe from "../hooks/useTicTacToe";
+import type { CSSProperties } from "react";
+
+const TicTacToe = ({boardSize = 3}) => {
+    const { board, resetGame, getStatusMessage, handleClick } = useTicTacToe(boardSize);
+  return (
+    <div className="game auto" style={{'--board-size': boardSize} as CSSProperties}>
+        <div className="status">
+            {getStatusMessage()}
+        <button className="reset-game" onClick={resetGame}>Reset Game</button>
+        </div>
+        <div className="board"> 
+            {
+                board.map((b, index) => <button className="cell" key={index} onClick={() => handleClick(index)} disabled={b !== null}>{b}</button>)
+            }
+        </div>
+    </div>
+  )
+}
+
+export default TicTacToe
