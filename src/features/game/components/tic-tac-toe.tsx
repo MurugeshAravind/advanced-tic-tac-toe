86b9a90d3ab6b2bec8f5ab27d54e3b559fcd87f2
Murugesh161192 <MBMurugesh.Aravind@cognizant.com>
2026-03-24 16:53:42 +0530

    test: trigger Gemini PR Review

--- src/features/game/components/tic-tac-toe.tsx
+++ src/features/game/components/tic-tac-toe.tsx
@@ -5,7 +5,7 @@ import GameStatus from '@/features/game/components/game-status';
 const TicTacToe = ({ boardSize }: { boardSize: number }) => {
     const { board, currentPlayer, winner, isDraw, isGameOver, winningCells, handleCellClick, resetGame } =
         useGameLogic(boardSize);
-
+    console.log({ board, currentPlayer, winner, isDraw, isGameOver, winningCells});
     return (
         <div className="game auto" style={{ maxWidth: `${boardSize * 100}px` }}>
             <GameStatus
